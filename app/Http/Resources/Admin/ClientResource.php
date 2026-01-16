f6afe49bd687ac55d28e1369349ceea2c1ca7616
ajboysanity07-gif <ajboysanity07@gmail.com>
2026-01-16 16:56:53 +0800

    Add storage URL generation for profile and document photos

--- app/Http/Resources/Admin/ClientResource.php
+++ app/Http/Resources/Admin/ClientResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\Admin;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class ClientResource extends JsonResource
 {
@@ -17,13 +18,25 @@ class ClientResource extends JsonResource
             'name' => $this->name,
             'email' => $this->email,
             'profile_picture_path' => $this->profile_picture_path,
+            'profile_picture_url' => $this->profile_picture_path
+                ? Storage::url($this->profile_picture_path)
+                : null,
             'phone_no' => $this->phone_no,
             'acctno' => $this->acctno,
             'status' => $this->status,
             'class' => $this->when(isset($this->loan_class), $this->loan_class),
             'prc_id_photo_front' => $this->prc_id_photo_front,
+            'prc_id_photo_front_url' => $this->prc_id_photo_front
+                ? Storage::url($this->prc_id_photo_front)
+                : null,
             'prc_id_photo_back' => $this->prc_id_photo_back,
+            'prc_id_photo_back_url' => $this->prc_id_photo_back
+                ? Storage::url($this->prc_id_photo_back)
+                : null,
             'payslip_photo_path' => $this->payslip_photo_path,
+            'payslip_photo_url' => $this->payslip_photo_path
+                ? Storage::url($this->payslip_photo_path)
+                : null,
             'created_at' => $this->created_at?->toISOString(),
             'reviewed_at' => $this->reviewed_at?->toISOString(),
             'reviewed_by' => $this->reviewed_by,
