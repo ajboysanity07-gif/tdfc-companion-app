4d936055a3737327c040f802304db8b31549cbb4
ajboysanity07-gif <ajboysanity07@gmail.com>
2026-01-30 10:23:27 +0800

    Fix route syntax: Use array notation for Email verification controllers

--- routes/auth.php
+++ routes/auth.php
@@ -58,8 +58,8 @@ Route::middleware(['auth'])
     ->name('client.registration-status');
 
 Route::middleware('auth')->group(function () {
-    Route::get('verify-email', EmailVerificationPromptController::class)->name('verification.notice');
-    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
+    Route::get('verify-email', [EmailVerificationPromptController::class, '__invoke'])->name('verification.notice');
+    Route::get('verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
         ->middleware(['signed', 'throttle:6,1'])
         ->name('verification.verify');
     Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
