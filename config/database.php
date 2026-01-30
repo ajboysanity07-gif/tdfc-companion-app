4d8306994418ebdb5afbfb7825200dda44cf2716
ajboysanity07-gif <ajboysanity07@gmail.com>
2026-01-30 10:54:01 +0800

    Simplify SQL Server config - remove problematic PDO options

--- config/database.php
+++ config/database.php
@@ -110,14 +110,8 @@ return [
             'prefix_indexes' => true,
             'encrypt' => env('DB_ENCRYPT', 'no'),
             'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', true),
-            'TrustServerCertificate' => env('DB_TRUST_SERVER_CERTIFICATE', true),
-            'Encrypt' => env('DB_ENCRYPT', 'no'),
-            // Connection timeout in seconds (default is 15)
-            'connect_timeout' => env('DB_CONNECT_TIMEOUT', 60),
-            'loginTimeout' => env('DB_LOGIN_TIMEOUT', 60),
-            'options' => [
-                \PDO::ATTR_TIMEOUT => env('DB_PDO_TIMEOUT', 60),
-            ],
+            'connect_timeout' => env('DB_CONNECT_TIMEOUT', 30),
+            'login_timeout' => env('DB_LOGIN_TIMEOUT', 30),
         ],
 
     ],
