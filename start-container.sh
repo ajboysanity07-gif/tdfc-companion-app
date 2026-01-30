7bc7ff6f28151fe9601e5647fbef4ffe44b31393
ajboysanity07-gif <ajboysanity07@gmail.com>
2026-01-30 12:35:20 +0800

    Add 2 second sleep before nginx starts to ensure PHP-FPM socket exists

--- start-container.sh
+++ start-container.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # Wait for Tailscale to connect
-echo "Waiting for Tailscale network..."
 for i in {1..30}; do
     if tailscale status | grep -q "100\."; then
-        echo "Tailscale connected"
         break
     fi
     sleep 1
@@ -12,6 +10,9 @@ done
 
 # Start PHP-FPM
 php-fpm -D
+
+# Wait for socket to be created
+sleep 2
 
 # Start nginx
 nginx -g "daemon off;"
