a4422e23ff7518cc1860910e203a979882de9df7
ajboysanity07-gif <ajboysanity07@gmail.com>
2026-02-12 19:14:17 +0800

    Add R2 filesystem disk and defaults

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DISK', 'local'),
+    'default' => env('FILESYSTEM_DISK', env('FILESYSTEM_CLOUD', 'local')),
 
     /*
     |--------------------------------------------------------------------------
@@ -49,13 +49,28 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'key' => env('AWS_ACCESS_KEY_ID', env('R2_ACCESS_KEY_ID')),
+            'secret' => env('AWS_SECRET_ACCESS_KEY', env('R2_SECRET_ACCESS_KEY')),
+            'region' => env('AWS_DEFAULT_REGION', env('R2_REGION', 'auto')),
+            'bucket' => env('AWS_BUCKET', env('R2_BUCKET')),
+            'url' => env('AWS_URL', env('R2_PUBLIC_BASE_URL')),
+            'endpoint' => env('AWS_ENDPOINT', env('R2_ENDPOINT')),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', env('R2_USE_PATH_STYLE_ENDPOINT', true)),
+            'visibility' => 'private',
+            'throw' => false,
+            'report' => false,
+        ],
+
+        'r2' => [
+            'driver' => 's3',
+            'key' => env('R2_ACCESS_KEY_ID', env('AWS_ACCESS_KEY_ID')),
+            'secret' => env('R2_SECRET_ACCESS_KEY', env('AWS_SECRET_ACCESS_KEY')),
+            'region' => env('AWS_DEFAULT_REGION', env('R2_REGION', 'auto')),
+            'bucket' => env('R2_BUCKET', env('AWS_BUCKET')),
+            'url' => env('R2_PUBLIC_BASE_URL', env('AWS_URL')),
+            'endpoint' => env('R2_ENDPOINT', env('AWS_ENDPOINT')),
+            'use_path_style_endpoint' => env('R2_USE_PATH_STYLE_ENDPOINT', env('AWS_USE_PATH_STYLE_ENDPOINT', true)),
+            'visibility' => 'private',
             'throw' => false,
             'report' => false,
         ],
